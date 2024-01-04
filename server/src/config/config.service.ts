b757cfde20979fd793fbc379cc18f604983cf23f
Deniseukaa <73753893+Deniseukaa@users.noreply.github.com>
2024-01-04 16:49:34 +0200

    feat: add logging to config service

--- server/src/config/config.service.ts
+++ server/src/config/config.service.ts
@@ -1,14 +1,16 @@
 import { DotenvConfigOutput, DotenvParseOutput, config } from 'dotenv';
 import { IConfigService } from './config.service.interface';
+import { LoggerService } from '../logger/logger.service';
 
 export class ConfigService implements IConfigService {
   private config: DotenvParseOutput;
 
-  constructor() {
+  constructor(private readonly logger: LoggerService) {
     const result: DotenvConfigOutput = config();
     if (result.error) {
-      console.log(result.error);
+      this.logger.error('Cant read .env file');
     } else {
+      this.logger.log('Config .env was loaded');
       this.config = result.parsed as DotenvParseOutput;
     }
   }
