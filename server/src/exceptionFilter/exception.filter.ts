33535e97b60e104c8fa92dd68418631bbbe77272
Deniseukaa <73753893+Deniseukaa@users.noreply.github.com>
2024-01-04 19:35:11 +0200

    feat: add exception filter and HttpError

---
+++ server/src/exceptionFilter/exception.filter.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+import { LoggerService } from '@/logger/logger.service';
+import { IExceptionFilter } from './exception.filter.interface';
+import { HTTPError } from '@common/errors/http.error';
+
+export class ExeptionFilter implements IExceptionFilter {
+  constructor(private readonly logger: LoggerService) {}
+  catch(
+    err: Error | HTTPError,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): void {
+    if (err instanceof HTTPError) {
+      this.logger.error(
+        `[${err.context}] Error ${err.statusCode} : ${err.message}`,
+      );
+      res.status(err.statusCode).send({ err: err.message });
+    } else {
+      this.logger.error(`${err.message}`);
+      res.status(500).send({ err: err.message });
+    }
+  }
+}
