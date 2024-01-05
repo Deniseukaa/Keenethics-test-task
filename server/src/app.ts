d14087022dd109ede934c8d9ef1eb062c1b65848
Deniseukaa <73753893+Deniseukaa@users.noreply.github.com>
2024-01-05 12:37:43 +0200

    feat: create server app instance and refactor main

---
+++ server/src/app.ts
@@ -0,0 +1,52 @@
+import express, { Express } from 'express';
+import { Server } from 'http';
+
+import { ExceptionFilter } from '@/exceptionFilter/exception.filter';
+import { BicycleController } from '@bicycles/bicycle.controller';
+import { MongoService } from '@/database/mongo.service';
+import { LoggerService } from '@/logger/logger.service';
+
+export class App {
+  app: Express;
+  port: number;
+  server: Server;
+
+  constructor(
+    private readonly logger: LoggerService,
+    private readonly bicycleController: BicycleController,
+    private readonly exceptionFilter: ExceptionFilter,
+    private readonly mongoService: MongoService,
+  ) {
+    this.app = express();
+    this.port = 3000;
+  }
+  useRoutes(): void {
+    this.app.use('/bicycles', this.bicycleController.router);
+  }
+
+  useMiddleWare(): void {
+    this.app.use(express.json());
+  }
+
+  useExeptionFilters(): void {
+    this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+  }
+
+  public async init(): Promise<void> {
+    this.useMiddleWare();
+    this.useRoutes();
+    this.useExeptionFilters();
+    await this.mongoService.connect();
+    this.server = this.app.listen(this.port);
+    this.logger.log(`Server starts on http://localhost:${this.port}`);
+  }
+
+  public close(): void {
+    this.server.close();
+  }
+}
+
+// TODO: refactor paths
+// TODO: add enums to status
+// TODO: commit all changes
+// TODO: mb add swagger
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,15 +1,24 @@
-import express, { Express, Request, Response } from 'express';
-import { Server } from 'http';
-const app: Express = express();
-const port = 8080;
-app.use(express.json());
-const server: Server = app.listen(port);
+import { App } from '@/app';
+import { ConfigService } from '@/config/config.service';
+import { MongoService } from '@/database/mongo.service';
+import { ExceptionFilter } from '@/exceptionFilter/exception.filter';
+import { LoggerService } from '@/logger/logger.service';
+import { BicycleController } from '@bicycles/bicycle.controller';
+import { BicycleModel } from '@bicycles/bicycle.model';
+import { BicycleService } from '@bicycles/bicycle.service';
 
-app.get('/', (req: Request, res: Response) => {
-	res.send('Welcome to Express & TypeScript Server');
-});
+async function bootstrap(): Promise<App> {
+  const logger = new LoggerService();
+  const configService = new ConfigService(logger);
+  const mongoService = new MongoService(configService, logger);
+  const exceptionFilter = new ExceptionFilter(logger);
 
-app.post('/', (req: Request, res: Response) => {
-	console.log(req.body);
-	res.send(req.body);
-});
+  const bicycleService = new BicycleService(logger, BicycleModel);
+  const bicycleController = new BicycleController(logger, bicycleService);
+
+  const app = new App(logger, bicycleController, exceptionFilter, mongoService);
+  await app.init();
+  return app;
+}
+
+export const boot = bootstrap();
